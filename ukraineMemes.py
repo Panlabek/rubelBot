28f2f8c00efef4dd4f3d0bcb93e32462bf671b83
Panlabek <marcin-herra@wp.pl>
2022-03-20 12:09:57 +0100

    Script that will retweet memes tweeted by Ukraine

--- ukraineMemes.py
+++ ukraineMemes.py
@@ -11,5 +11,15 @@ client = tweepy.Client(bearer_token=BEARER_TOKEN,
                        consumer_key=CONSUMER_KEY,
                        consumer_secret=CONSUMER_SECRET)
 
+# to be continued
 def ukraineMemes():
-    pass
+    resp = client.search_recent_tweets(query="from:Ukraine -is:reply lang:en", tweet_fields=["text", "source", "context_annotations", "created_at"], max_results=10, # add start time to 1 minute
+                                       media_fields=["preview_image_url"], expansions="attachments.media_keys")
+    media = {m["media_key"]: m for m in resp.includes["media"]}
+    for tweet in resp.data:
+        if "attachments" in tweet.data:
+            attachments = tweet.data["attachments"]
+            media_keys = attachments["media_keys"]
+            if media[media_keys[0]].preview_image_url:
+                print(media[media_keys[0]].preview_image_url)
+ukraineMemes()
