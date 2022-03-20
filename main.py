c58688988c350d30aaccc7f1d1bbdb4e8cebe2b6
Panlabek <marcin-herra@wp.pl>
2022-03-20 12:11:54 +0100

    Added necessary things to main for it to work

--- main.py
+++ main.py
@@ -5,9 +5,6 @@
 # OPTIONAL
 # add it to database
 # TODO 2 
-# check if ukraine has some memes posted
-# if yes retweet it
-# TODO 3 
 # if its week
 # create twitter post that has 
 # rubel/usd price
@@ -16,9 +13,18 @@
 # add matplotlib chart to media and add random frog meme
 # if weekend only post memes and retweet ukraine
 # if someone replies to post, reply with a meme to him
+# TODO 3
+# check if ukraine has some memes posted
+# if yes retweet it
+# TODO 4
+# pepe.py that generates random pepe image
+# TODO 5
+# place id randomly generates place names
 #===================================================================
 import datetime
 from rubelFetcher import dataFetcher
+from pepeRandomGenerator import getRandomPepe
+from tweetPoster import rubTweetPoster
 
 def main():
     # start_date = str(datetime.datetime.now())[:10].split("-")
@@ -26,8 +32,11 @@ def main():
     # start_date = "-".join(start_date)
     start_date = "2020-01-1"
     end_date = str(datetime.datetime.now())[:10]
-    resp = dataFetcher(start_date, end_date)
-    print(resp)
+    rubPrice = dataFetcher(start_date, end_date)
+    pepe_path = getRandomPepe()
+    matplotlibImg = "RUB|USD2022-03-20.png"
+    rubTweetPoster(rub_price=rubPrice, matplotlib_img=matplotlibImg,
+                   pepe_image=pepe_path, place_id="Goblin Town")
 
 if __name__ == "__main__":
     main()
