3f82e69e5b48d4f817d02f0ceab2cdcf8bf76634
Panlabek <marcin-herra@wp.pl>
2022-03-20 20:58:04 +0100

    Fixed saved data name to only YYYY-MM-DD, added colors to plot

--- rubelFetcher.py
+++ rubelFetcher.py
@@ -1,10 +1,19 @@
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
+import random
+
+COLORS = ["midnightblue", "navy", "darkblue", "mediumblue", "blue", "slateblue", "darkslateblue", "mediumslateblue", "mediumpurple",
+        "rebeccapurple", "blueviolet", "indigo", "darkorchid", "darkviolet", "mediumorchid", "thistle", "plum", "violet", "purple",
+        "darkmagenta", "fuchsia", "magenta", "orchid", "mediumvioletred", "deeppink", "hotpink", "lavenderblush", "palevioletred",
+        "crimson", "pink", "lightpink", "ivory", "teal", "darkslategray", "dodgerblue", "steelblue", "lightslategray", "royalblue",
+        "ghostwhite", "lavender", "cornflowerblue"]
 
 def dataFetcher(start_date: str, end_date) -> str:
+    color_id = random.randrange(0,40)
+    color = COLORS[color_id]
     data = web.DataReader(name="RUBUSD=X", data_source="yahoo", start=start_date, end=end_date)
-    plt.title("RUBEL/USD")
-    plt.plot(data["Adj Close"])
-    plt.savefig(f"RUB|USD{end_date}.png")
-    data.to_csv(f"RUB|USD{end_date}.csv")
+    plt.title("RUB/USD")
+    plt.plot(data["Adj Close"], color=color)
+    plt.savefig(f"RUB|USD{str(end_date)[:10]}.png")
+    data.to_csv(f"RUB|USD{str(end_date)[:10]}.csv")
     return str(data["Adj Close"][-1])[:11]
