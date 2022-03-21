3a3ff2451ec217786cbd7e613d2dd330adcdd711
Panlabek <marcin-herra@wp.pl>
2022-03-21 18:49:38 +0100

    Made random numbers specific for file extension

--- pepeRandomGenerator.py
+++ pepeRandomGenerator.py
@@ -3,5 +3,10 @@ import random
 def getRandomPepe() -> str:
     extensions_pos = ["png", "jpg", "gif"] 
     ext_num = random.randrange(0,2)
-    num = random.randrange(1,900)
+    if ext_num == 1:
+        num = random.randrange(1,1258)
+    elif ext_num == 0:
+        num = random.randrange(1,897)
+    else:
+        num = random.randrange(1, 99)
     return f"pepe_photos/{num}{extensions_pos[ext_num]}.{extensions_pos[ext_num]}"
