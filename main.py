b5525ab0ebefe33efa7e8c1868b2caef62ae5a7d
Panlabek <marcin-herra@wp.pl>
2022-03-23 19:35:31 +0100

    Moved the managing of pepe memes directly intro tweetPoster

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
 
 from rubelFetcher import dataFetcher
-from pepeRandomGenerator import getRandomPepe
 from tweetPoster import rubTweetPoster
 from placeGenerator import getRandomUkrainianCity
 from replyReplier import replyToReplies
@@ -20,16 +19,15 @@ def main():
                 start_date = datetime.datetime.now() - datetime.timedelta(days=365) 
                 end_date = datetime.datetime.now()
                 rubPrice = dataFetcher(start_date, end_date)
-                pepe_path = getRandomPepe()
                 random_place = getRandomUkrainianCity()
                 matplotlibImg = f"RUB|USD{str(end_date)[:10]}.png"
                 tweets.append("Tweeted")
                 rubTweetPoster(rub_price=rubPrice, matplotlib_img=matplotlibImg,
-                               pepe_image=pepe_path, place_id=random_place)
+                               place_id=random_place)
             if int(datetime.datetime.now().astimezone(pytz.utc).strftime("%H")) == 14 and len(tweets) > 0:
                 tweets.clear()
             if int(datetime.datetime.now().astimezone(pytz.utc).strftime("%M")) % 2 == 0 and int(datetime.datetime.now().astimezone(pytz.utc).strftime("%S")) == 0:
-                replyToReplies(getRandomPepe(), getRandomPepe(), usedTweetList)
+                replyToReplies(usedTweetList)
                  
     except KeyboardInterrupt:
         print("Bot has been stopped")
--- tweetPoster.py
+++ tweetPoster.py
@@ -1,6 +1,7 @@
 import tweepy
 from priceConverter import convertFromRtoP, convertFromRtoE
 from decouple import config
+from pepeRandomGenerator import getRandomPepe
 CONSUMER_KEY = config("CONSUMER_KEY")
 CONSUMER_SECRET = config("CONSUMER_SECRET")
 ACCESS_KEY = config("ACCESS_KEY")
@@ -9,14 +10,14 @@ ACCESS_SECRET = config("ACCESS_SECRET")
 auth=tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api=tweepy.API(auth)
-def rubTweetPoster(rub_price: str, matplotlib_img: str, pepe_image: str, place_id: str):
+def rubTweetPoster(rub_price: str, matplotlib_img: str, place_id: str):
     # client.create_tweet(text=f"RUB/USD price: {rub_price}", place_id=place_id) 
     gbp_converted = convertFromRtoP(rubelPrice=rub_price)
     eur_converted = convertFromRtoE(rubelPrice=rub_price)
     tweet_text = f"RUB-USD price: {rub_price}\n" \
                 f"RUB-EUR price: {eur_converted}\n" \
                 f"RUB-GBP price: {gbp_converted}"
-    images = (pepe_image, matplotlib_img)
+    images = (getRandomPepe(), matplotlib_img)
     media_ids = [api.media_upload(i).media_id_string for i in images]
     tweet = api.update_status(media_ids=media_ids, status=tweet_text, place_id=place_id)
     return tweet
