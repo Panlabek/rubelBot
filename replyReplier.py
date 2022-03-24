094af48aed932f40da46e8ef1900cff42618a581
Panlabek <marcin-herra@wp.pl>
2022-03-24 14:42:04 +0100

    Replies generate pepes directly in function, posted reply is also getting a like from bot

--- replyReplier.py
+++ replyReplier.py
@@ -1,6 +1,7 @@
 import datetime
 import tweepy
 from decouple import config
+from pepeRandomGenerator import getRandomPepe
 CONSUMER_KEY = config("CONSUMER_KEY")
 CONSUMER_SECRET = config("CONSUMER_SECRET")
 ACCESS_KEY = config("ACCESS_KEY")
@@ -16,7 +17,7 @@ auth=tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SEC
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api=tweepy.API(auth)
 
-def replyToReplies(pepe_image: str, pepe_image2: str, usedTweets: list[str]) -> str:
+def replyToReplies(usedTweets: list[str]) -> str:
     minuser = datetime.timedelta(minutes=70) 
     start_time = datetime.datetime.now() - minuser
     tweets = client.search_recent_tweets(query="to:0xpiplup", start_time=start_time) 
@@ -24,6 +25,7 @@ def replyToReplies(pepe_image: str, pepe_image2: str, usedTweets: list[str]) -> 
         for tweet in tweets.data:
             if tweet.id not in usedTweets:
                 usedTweets.append(tweet.id)
-                images = (pepe_image, pepe_image2)
+                images = (getRandomPepe(), getRandomPepe())
                 media_ids = [api.media_upload(i).media_id_string for i in images]
+                client.like(tweet_id=tweet.id)
                 client.create_tweet(in_reply_to_tweet_id=tweet.id, media_ids=media_ids)
