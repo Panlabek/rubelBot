066801f9a3c9abfcf936ab88e5a7cb3c1122f935
Panlabek <marcin-herra@wp.pl>
2022-03-25 18:52:14 +0100

    Changed fixed number of images per extensions to dynamic number depending on real count of images

--- pepeRandomGenerator.py
+++ pepeRandomGenerator.py
@@ -1,12 +1,13 @@
 import random
+import os
 
 def getRandomPepe() -> str:
     extensions_pos = ["png", "jpg", "gif"] 
     ext_num = random.randrange(0,2)
-    if ext_num == 1:
-        num = random.randrange(1,1258)
-    elif ext_num == 0:
-        num = random.randrange(1,897)
+    if ext_num == 0:
+        num = random.randrange(1,len([name for name in os.listdir("pepe_photos") if ".png" in name]))
+    elif ext_num == 1:
+        num = random.randrange(1,len([name for name in os.listdir("pepe_photos") if ".jpg" in name]))
     else:
-        num = random.randrange(1, 99)
+        num = random.randrange(1,len([name for name in os.listdir("pepe_photos") if ".gif" in name]))
     return f"pepe_photos/{num}{extensions_pos[ext_num]}.{extensions_pos[ext_num]}"
