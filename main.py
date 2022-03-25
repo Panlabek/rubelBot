38ab6696b319c11c9354306e55560481b5bfb66f
Panlabek <marcin-herra@wp.pl>
2022-03-25 18:52:40 +0100

    Cleaned some of the duplicated code

--- main.py
+++ main.py
@@ -8,29 +8,25 @@ from replyReplier import replyToReplies
 import pytz
 
 def main():
-    tweets = []
-    usedTweetList = []
+    tweets, usedTweetList = [], []
     try:
         while True:
-            if int(datetime.datetime.now().astimezone(pytz.utc).strftime("%H")) == 13 and len(tweets) == 0:
-                start_date = str(datetime.datetime.now())[:10].split("-")
-                start_date[2] = str(int(start_date[2]) - 15)
-                start_date = "-".join(start_date)
+            utc_time = datetime.datetime.now().astimezone(pytz.utc)
+            if int(utc_time.strftime("%H")) == 13 and len(tweets) == 0:
                 start_date = datetime.datetime.now() - datetime.timedelta(days=365) 
                 end_date = datetime.datetime.now()
                 rubPrice = dataFetcher(start_date, end_date)
-                random_place = getRandomUkrainianCity()
                 matplotlibImg = f"RUB|USD{str(end_date)[:10]}.png"
                 tweets.append("Tweeted")
                 rubTweetPoster(rub_price=rubPrice, matplotlib_img=matplotlibImg,
-                               place_id=random_place)
-            if int(datetime.datetime.now().astimezone(pytz.utc).strftime("%H")) == 14 and len(tweets) > 0:
+                               place_id=getRandomUkrainianCity())
+            if int(utc_time.strftime("%H")) == 14 and len(tweets) > 0:
                 tweets.clear()
-            if int(datetime.datetime.now().astimezone(pytz.utc).strftime("%M")) % 2 == 0 and int(datetime.datetime.now().astimezone(pytz.utc).strftime("%S")) == 0:
+            if int(utc_time.strftime("%M")) % 2 == 0 and int(utc_time.strftime("%S")) == 0:
                 replyToReplies(usedTweetList)
                  
     except KeyboardInterrupt:
-        print("Bot has been stopped")
+        print("Bot has been turned off")
 
 if __name__ == "__main__":
     main()
