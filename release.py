3a4edd942332f115927b5dd9463a2a85c786e909
Panlabek <marcin-herra@wp.pl>
2022-03-27 16:48:35 +0200

    Added hour to dockerfile name

--- release.py
+++ release.py
@@ -1,9 +1,12 @@
 #!/bin/python3.10
 import subprocess
 import datetime
+import datetime
 
 destinedDirectory = "~/Documents/rubelArchives"
 
+hour = datetime.datetime.now().stftime("%H")
+
 today = datetime.datetime.now().strftime("%d")
-subprocess.run(["docker", "build", "-t", f"rubel-bot{today}", "."])
+subprocess.run(["docker", "build", "-t", f"rubel-bot{today}{hour}", "."])
 subprocess.run(f"docker save rubel-bot{today} > {destinedDirectory}/rubel-bot{today}.tar", shell=True)
